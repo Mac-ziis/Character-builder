427ef4b20931b2c36625f278728f6155fd58954e
Mac-ziis <macg286@gmail.com>
2023-06-14 16:52:38 -0700

    Add the event listener WIP: javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
 window.onload = function() {
   const form = document.querySelector("form");
-  form.onsubmit= function (event) {
+  form.onsubmit = function (event) {
       event.preventDefault();
-  
-
   }
+
+  function hendleCharacter(event) {
+    
+  }
+
+  window.addEventListener("load", function() {
+    console.log("Page loaded");
+    const form = document.getElementById("character");
+    form.addEventListener("submit", )
+  });
 }
+
+
